6a024512ed0e5fb60daa52dab9efb0e788cd279d
Тютин Руслан <RITyutin@sberbank.ru>
2021-03-28 10:09:55 +0300

    3 task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,19 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #limit = 0;
+  #size = 0;
   constructor(options) {
     super(options);
+    this.#limit = options.limit
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.#size += chunk.length
+    if (this.#limit !== 0 && this.#size > this.#limit) {
+      callback(new LimitExceededError(), "")
+    }
+    callback(undefined, chunk.toString())
   }
 }
 
