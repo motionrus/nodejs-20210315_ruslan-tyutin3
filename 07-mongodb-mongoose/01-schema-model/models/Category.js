f03c21c46e4378896654ccbc9cfe5367e3d1dc3b
Тютин Руслан <RITyutin@sberbank.ru>
2021-04-05 08:31:55 +0300

    07-1 solved

--- 07-mongodb-mongoose/01-schema-model/models/Category.js
+++ 07-mongodb-mongoose/01-schema-model/models/Category.js
@@ -2,10 +2,18 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  }
 });
 
 const categorySchema = new mongoose.Schema({
   subcategories: [subCategorySchema],
+  title: {
+    type: String,
+    required: true,
+  }
 });
 
 module.exports = connection.model('Category', categorySchema);
